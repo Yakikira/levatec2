03e124455a2bbb817d1876e045d0e261a4352165
Yakikira <yakikira@yahoo.co.jp>
2022-07-16 00:53:19 +0000

    10件表示に変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,6 @@ class PostController extends Controller
     }*/
     public function index(Post $post)
     {
-        return view("posts/index")->with(['posts' => $post->get()]);
+        return view("posts/index")->with(['posts' => $post->getbyLimit()]);
     }
 }
